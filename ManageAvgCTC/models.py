fe4e1f5f98d4a97110a9f1e7452f79f8d71d883b
Batchu <sbatchu@belcan.com>
2022-12-05 14:53:06 +0530

    updated Bill Rate by siva

--- ManageAvgCTC/models.py
+++ ManageAvgCTC/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 class AvgCTC(models.Model):
     Id = models.AutoField(primary_key=True, db_column='ID')
+    CompanyId = models.IntegerField(null=False, db_column='Company_ID')
+    BusinessUnitId = models.IntegerField(null=False, db_column='Business_Unit_ID')
     ServiceLineId = models.IntegerField(null=False, db_column='Service_Line_ID')
     ExperienceLevelId = models.IntegerField(null=False, db_column='Experience_Level_ID')
     AvgApprovedCTC = models.FloatField(null=False, db_column='Avg_Approved_CTC')
-    AvgBillRate = models.FloatField(null=True, db_column='Avg_Bill_Rate')
+    AvgBillRate = models.FloatField(null=False, db_column='Avg_Bill_Rate')
 
     class Meta:    
         db_table = 'AvgCTC'
+        unique_together = ('CompanyId', 'BusinessUnitId', 'ServiceLineId', 'ExperienceLevelId')
