26b7873126dbe03af825cec540748113fda5ef79
sbatchu <sbatchu@belcan.com>
2023-01-24 17:30:36 +0530

    R& D on Document template

--- pdf/views.py
+++ pdf/views.py
@@ -17,6 +17,10 @@ from datetime import datetime
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from fpdf import FPDF,HTMLMixin
+from docx import Document
+import re
+from docxtpl import DocxTemplate
+
 
 # Create your views here.
 class PDFGeneration(ModelViewSet):
@@ -170,65 +174,120 @@ class PDFGeneration(ModelViewSet):
         return HttpResponse(data)
 
     @action(detail=True, methods=['post'])
-    def offerletterpdf(self, request, format=None):
-        data = (
-            ("First name", "Last name", "Age", "City"),
-            ("Jules", "Smith", "34", "San Juan"),
-            ("Mary", "Ramos", "45", "Orlando"),
-            ("Carlson", "Banks", "19", "Los Angeles"),
-            ("Lucas", "Cimon", "31", "Saint-Mahturin-sur-Loire"),
-        )
-        pdf = PDF('P', 'mm', 'A4')
-        pdf.add_page()
-        pdf.set_margins(0, 0, 0)
+    def offerletterpdf(self, request, format=None):   
 
-        pdf.set_font('Times', '', 12)
-        pdf.cell(210, 10, 'Powered by FPDF.', 0, 0, 'C')
-        pdf.write_html(
-   """
-<H1 align="center">html2fpdf</H1>
-<h2>Basic usage</h2>
-<p>You can now easily print text mixing different
-styles : <B>bold</B>, <I>italic</I>, <U>underlined</U>, or
-<B><I><U>all at once</U></I></B>!<BR>You can also insert links
-on text, such as <A HREF="http://www.fpdf.org">www.fpdf.org</A>,
-or on an image: click on the logo.<br>
-<center>
-<A HREF="http://www.fpdf.org"><img src="tutorial/logo.png" width="104" height="71"></A>
-</center>
-<h3>Sample List</h3>
-<ul><li>option 1</li>
-<ol><li>option 2</li></ol>
-<li>option 3</li></ul>
+        buffer = io.BytesIO() 
+        doc = DocxTemplate("Belcan India Offer Letter.docx")
+        context = { 'Designation' : 'Software Engineer',
+            'Name' : 'Siva',
+            'isstatutory' : 'no',
+            'ispar' : 'no',
+            'tbl_contents': [
+        {'label': 'yellow', 'cols': ['banana', 'capsicum', 'pyrite', 'taxi']},
+        {'label': 'red', 'cols': ['apple', 'tomato', 'cinnabar', 'doubledecker']},
+        {'label': 'green', 'cols': ['guava', 'cucumber', 'aventurine', 'card']},
+            ],
+         'tbl_contents1': [
+        {'label': 'yellow', 'cols': ['banana', 'capsicum', 'pyrite', 'taxi']},
+        {'label': 'red', 'cols': ['apple', 'tomato', 'cinnabar', 'doubledecker']},
+        {'label': 'green', 'cols': ['guava', 'cucumber', 'aventurine', 'card']},
+            ]   }
+        doc.render(context)
+        # doc.save("generated_doc.docx")  
+        doc.save(buffer)
+        buffer.seek(0)
+        data = base64.b64encode(buffer.getvalue())
+        return HttpResponse(data)
+    # @action(detail=True, methods=['post'])
+    # def offerletterpdf(self, request, format=None):
+    #     file_path = 'Belcan India Offer Letter.docx'
+    #     if file_path.endswith('.docx'):
+    #         doc = Document(file_path)
+    #         # Loop through replacer arguments
 
-<table border="0" align="center" width="50%">
-<thead><tr><th width="30%">Header 1</th><th width="70%">header 2</th></tr></thead>
-<tbody>
-<tr><td>cell 1</td><td>cell 2</td></tr>
-<tr><td>cell 2</td><td>cell 3</td></tr>
-</tbody>
-</table>
-"""
+    #         for para in doc.paragraphs:
+    #             # Loop through runs (style spans)
+    #             for run in para.runs:
+    #                 # if there is text on this run, replace it
+    #                 if run.text:
+    #                     # get the replacement text
+    #                     replaced_text = re.sub('Designation', 'Software Engineer', run.text, 999)
+    #                     if replaced_text != run.text:
+    #                         # if the replaced text is not the same as the original
+    #                         # replace the text and increment the number of occurences
+    #                         run.text = replaced_text
+                   
+                            
 
-            )
-        pdf.output('sample.pdf', 'F')  
-        return HttpResponse("asasas")
+            
+    #         # make a new file name by adding "_new" to the original file name
+    #         new_file_path = file_path.replace(".docx", "_new.docx")
+    #         # save the new docx file
+    #         doc.save(new_file_path)
+    #     else:
+    #         print('The file type is invalid, only .docx are supported')
 
-class PDF(FPDF, HTMLMixin):
-    def header(self):
-        # Select Arial bold 15
-        self.set_font('Arial', 'B', 15)
-        # Move to the right
-        self.cell(80)
-        # Framed title
-        self.cell(30, 10, 'Title', 1, 0, 'C')
-        # Line break
-        self.ln(20)
 
-    def footer(self):
-        # Go to 1.5 cm from bottom
-        self.set_y(-15)
-        # Select Arial italic 8
-        self.set_font('Arial', 'I', 8)
-        # Print centered page number
-        self.cell(0, 10, 'Page %s' % self.page_no(), 0, 0, 'C')
+#     @action(detail=True, methods=['post'])
+#     def offerletterpdf(self, request, format=None):
+#         data = (
+#             ("First name", "Last name", "Age", "City"),
+#             ("Jules", "Smith", "34", "San Juan"),
+#             ("Mary", "Ramos", "45", "Orlando"),
+#             ("Carlson", "Banks", "19", "Los Angeles"),
+#             ("Lucas", "Cimon", "31", "Saint-Mahturin-sur-Loire"),
+#         )
+#         pdf = PDF('P', 'mm', 'A4')
+#         pdf.add_page()
+#         pdf.set_margins(0, 0, 0)
+
+#         pdf.set_font('Times', '', 12)
+#         pdf.cell(210, 10, 'Powered by FPDF.', 0, 0, 'C')
+#         pdf.write_html(
+#    """
+# <H1 align="center">html2fpdf</H1>
+# <h2>Basic usage</h2>
+# <p>You can now easily print text mixing different
+# styles : <B>bold</B>, <I>italic</I>, <U>underlined</U>, or
+# <B><I><U>all at once</U></I></B>!<BR>You can also insert links
+# on text, such as <A HREF="http://www.fpdf.org">www.fpdf.org</A>,
+# or on an image: click on the logo.<br>
+# <center>
+# <A HREF="http://www.fpdf.org"><img src="tutorial/logo.png" width="104" height="71"></A>
+# </center>
+# <h3>Sample List</h3>
+# <ul><li>option 1</li>
+# <ol><li>option 2</li></ol>
+# <li>option 3</li></ul>
+
+# <table border="0" align="center" width="50%">
+# <thead><tr><th width="30%">Header 1</th><th width="70%">header 2</th></tr></thead>
+# <tbody>
+# <tr><td>cell 1</td><td>cell 2</td></tr>
+# <tr><td>cell 2</td><td>cell 3</td></tr>
+# </tbody>
+# </table>
+# """
+
+#             )
+#         pdf.output('sample.pdf', 'F')  
+#         return HttpResponse("asasas")
+
+# class PDF(FPDF, HTMLMixin):
+#     def header(self):
+#         # Select Arial bold 15
+#         self.set_font('Arial', 'B', 15)
+#         # Move to the right
+#         self.cell(80)
+#         # Framed title
+#         self.cell(30, 10, 'Title', 1, 0, 'C')
+#         # Line break
+#         self.ln(20)
+
+#     def footer(self):
+#         # Go to 1.5 cm from bottom
+#         self.set_y(-15)
+#         # Select Arial italic 8
+#         self.set_font('Arial', 'I', 8)
+#         # Print centered page number
+#         self.cell(0, 10, 'Page %s' % self.page_no(), 0, 0, 'C')
